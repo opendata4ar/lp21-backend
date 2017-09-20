9e6dd459777e299fe5a40cf1ce00d6a942e74a60
opendata 4 ar <opendata4ar@gmail.com>
2017-09-20 23:07:13 +0200

    get cities from sql to client

--- lp21-backend.js
+++ lp21-backend.js
@@ -5,7 +5,7 @@ var format = require('pg-format')
 const myport = 8081
 const myip   = '0.0.0.0'
 
-const { Client } = require('pg')
+const { Client } = require('pg');
 
 const client = new Client({
   host: 'lp21.cyi5isrniwic.eu-west-1.rds.amazonaws.com',
@@ -17,29 +17,67 @@ const client = new Client({
 
 client.connect((err) => {
   if (err) {
-    console.error('connection error', err.stack)
+    console.error('connection error', err.stack);
   } else {
-    console.log('connected to DB at ' + client.host)
+    console.log('connected to DB at ' + client.host);
   }
 })
 
-const qCities = {
-  // give the query a unique name
-  name: 'get-all-cities',
+const qCitiesByState = {
+  // give the query a *unique* name
+  name: 'get-cities-by-state',
   text: 'SELECT * FROM city WHERE kanton_id = $1',
   //rowMode: 'array',
   values: [21]
 }
 
-app.get("/lp21/:id",function(request, response){
-  var kanton_id = request.params.id;
-  qCities.values = [kanton_id];
-  client.query(qCities, (err, res) => {
-    if (err) { console.log(err.stack)
-    } else { //console.log(res.rows)
+const qCitiesByUser = {
+	  name: 'get-cities-by-state', // id isNaN
+	  text: "SELECT * FROM city, ... WHERE email = '$1'",
+	  //rowMode: 'array',
+	  values: ['opendata4ar@gmail.com']
+	}
+
+const qCities = {
+	  name: 'get-all-cities',
+	  text: 'SELECT * FROM city',
+	}
+
+app.get("/lp21/city/:id",function(httpRequest, httpResponse){
+  var kanton_id = httpRequest.params.id;
+  if (kanton_id == undefined || kanton_id < 1 ) {
+	var sql = qCities;
+  } else {
+	qCitiesByState.values = [kanton_id];
+	var sql = qCitiesByState;
+  }
+  //qCities.values = [kanton_id];
+  client.query(sql, (sqlError, sqlResult) => {
+    if (sqlError) { console.log(sqlError.stack);
+      httpResponse.header("Access-Control-Allow-Origin", "*");
+	  var errMsg = "{'errorMessage': '" + sqlError.message + "', 'errorStack': '" + sqlError.stack + "'}";
+      httpResponse.header('Content-Length', errMsg.length);
+      httpResponse.header('Transfer-Encoding', '');
+      httpResponse.writeHead(500, {"Content-Type": "application/json"});
+      httpResponse.write(errMsg);  
+
+    } else { //console.log(sqlResult.rows)
       //TODO: transform to <li>? var obj = { id : kanton_id, Content : "lp21  " +id };
-      response.writeHead(200, {"Content-Type": "application/json"});
-      response.write(JSON.stringify(res.rows));  
+      var jsonResult = JSON.stringify(sqlResult.rows);
+      httpResponse.header("Access-Control-Allow-Origin", "*");
+      
+      // Need to set Content-Length explicitly just to avoid Transfer-Encoding: chunked
+      //JS string.length does not account for utf-8 encoding
+      // see https://stackoverflow.com/questions/5515869/string-length-in-bytes-in-javascript
+      // Matches only the 10.. bytes that are non-initial characters in a multi-byte sequence.
+      var utf8ExtraLength = encodeURIComponent(jsonResult).match(/%[89ABab]/g);
+      var contentLength = jsonResult.length + (utf8ExtraLength ? utf8ExtraLength.length : 0);
+      console.log("len="+jsonResult.length+" or (utf-8) "+contentLength+"?\n"+jsonResult);
+
+      httpResponse.header('Content-Length', contentLength);
+      httpResponse.header('Transfer-Encoding', '');
+      httpResponse.writeHead(200, {"Content-Type": "application/json"});
+      httpResponse.write(jsonResult);
     }
    })   
 });
@@ -48,9 +86,13 @@ app.get("/lp21/:id",function(request, response){
 
 
 client.on('notice', (msg) => console.warn('notice:', msg))
-client.on('error', (err) => {
-  console.error('something bad has happened!', err.stack)
-  res.status(500).send('Something bad happened!');
+client.on('error', (error) => {
+  console.error('something bad has happened!', error.stack)
+  var errMsg = "{'errorMessage': '" + error.message + "', 'errorStack': '" + error.stack + "'}";
+  httpResponse.header('Content-Length', errMsg.length);
+  httpResponse.header('Transfer-Encoding', '');
+  httpResponse.writeHead(500, {"Content-Type": "application/json"});
+  httpResponse.write(errMsg);  
 })
 
 /*
@@ -63,9 +105,13 @@ client.end((err) => {
 */
 
 // error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
+app.use(function(error, req, res, next){
+  console.error(error.stack);
+  var errMsg = "{'errorMessage': '" + error.message + "', 'errorStack': '" + error.stack + "'}";
+  httpResponse.header('Content-Length', errMsg.length);
+  httpResponse.header('Transfer-Encoding', '');
+  httpResponse.writeHead(500, {"Content-Type": "application/json"});
+  httpResponse.write(errMsg);  
 });
 
 
